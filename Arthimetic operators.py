6ce588ec78350531aa8997ad1441bcca95b35609
MEENA@09 <meenakshikasilinka@gmail.com>
2025-07-30 14:20:32 +0530

    Add files via upload

---
+++ Arthimetic operators.py
@@ -0,0 +1,16 @@
+"""
+an arthimetic operator is used to perform some mathematical \arthimetic 
+the operators are:
++,-,*,/,//,%,**
+NOTE:input() is a function which takes input from user
+code 
+"""
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))  
+print("the addition",a+b)
+print("the subtraction",a-b)
+print("the multiplication",a*b)
+print("the division",a/b)#which returns the  quotient 
+print("the floordivision",a//b)#which returns the integral quotient part in a division
+print("the modulardivision",a%b)#which returns the remainder part before the decimal
+print("the power",a**b)#which returns the power`
